cba5ed0c7af384237d10f5fc2efdc4e3d6154e8f
Jakub Nabaglo <jakub.nabaglo@data61.csiro.au>
2018-12-26 20:53:54 +1100

    readme and completed solutions

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # rl-tic-tac-toe
-Idea for an introductory reinforcement learning project for high schoolers
+
+This is an idea for a project that teaches basic reinforcement learning to high schoolers. At one stage I was planning it for an [NCSS](https://ncss.edu.au/summer-school) Masterclass, but it may be too complicated.
+
+It is aimed at bright students, who know basic Python and high-school level maths. It is dependent on base Python 3 only and does not require third-party packages. Students should be able to complete this in a day from scratch, or a few hours if they are given a template that implements the boring bits.
+
+Students will learn the basic concepts of reinforcement learning (agent, state, action, reward) as well as basic Q-learning.
+
+Of course, students vary in their capabilities. We need to cater to students who are beginners to computer science, as well as to more experienced programmers (without anyone getting lost or bored). There are multiple stages to the exercise. A student who completes at least two can say that they used reinforcement learning to play tic-tac-toe!
+
+Those stages are described below. Sample solutions are included in the repo. I had fun implementing the solutions in ways I thought were neat. I expect students to not know Python&rsquo;s more advanced functionality.
+
+# Stages
+
+## Stage 1: interface
+The students make a framework for playing tic-tac-toe. They are able to play a game by displaying the current board, asking the user for a move, validating this move, and applying it. The opponent is a basic agent that makes any valid move.
+
+This is a good place to introduce the terms &lsquo;agent&rsquo;, &lsquo;state&rsquo;, and &lsquo;action&rsquo;.
+
+At this stage it is good to nudge the students towards giving the human player and the basic AI player a common interface. This will later let us play two RL players against each other using the same framework.
+
+## Stage 2: basic RL agent
+The basic RL agent is given a state _s_, chooses an action _a_, and is given a reward _r_. _r_ = 1 if the action causes the agent to win the game, _r_ = &minus;1 if the game is lost, and _r_ = 0 otherwise. It stores a table Q: state &times; action &rarr; R and picks _a_ that maximises _Q_(_s_, _a_) for our fixed _s_.
+
+Once we get the reward the update step is:
+_Q_(_s_, _a_) := _r_ &minus; max<sub>_a_&prime;</sub>_Q_(_s_&prime;, _a_&prime;), where _s_&prime; is our new state and _a_&prime; is any possible action from _s_&prime;. If there are no possible actions, we set the min term to 0. You may recognise this as a special case of _Q_-learning where we set both the learning rate _&alpha;_ and the discount rate _&gamma;_. We replace adding max with subtracting it, because there are two agents playing against each other (this is analogous to the [Minimax algorithm](https://en.wikipedia.org/wiki/Minimax)).
+
+The action that we choose, _a_, is the one with the highest value _Q_(_s_, _a_). We tiebreak randomly.
+
+We should theoretically be able to train our agent by playing against it repeatedly. Note that it may take a while to train, so it&rsquo;s fairly infeasible to do by hand. So we play it off against itself.
+
+## Stage 3:
+Three components that as extensions for faster students:
+- We introduce _&epsilon;_-greedy and have agent choose a random action _&epsilon;_ of the time when training. This is a good time to talk about exploration vs exploitation.
+- We introduce the learning rate _&alpha;_ and discount rate _&gamma;_. Our update formula then becomes _Q_(_s_, _a_) := (1 &minus; _&alpha;_) _Q_(_s_, _a_) + _&alpha;_ (_r_ &minus; _&gamma;_ max<sub>_a_&prime;</sub>Q(_s_&prime;, _a_&prime;)).
+- We use symmetry to collapse equivalent states.
---
+++ stage2.py
@@ -0,0 +1,266 @@
+import random
+
+# Global for persistance between episodes
+Q = {}
+
+
+class GameFinished(Exception):
+    """Thrown into the corourine when the game is over."""
+    def __init__(self, winning_symbol):
+        """winning_symbol = None for a draw"""
+        self.winning_symbol = winning_symbol
+
+
+class IllegalMove(Exception):
+    """Thrown into the coroutine when the move is invalid."""
+
+
+def board_to_strs(board):
+    return [[' ' if cell is None else cell for cell in row] for row in board]
+
+
+def print_board(board):
+    board_strs = board_to_strs(board)
+    print('  1   2   3')
+    print(f'A {board_strs[0][0]} | {board_strs[0][1]} | {board_strs[0][2]}')
+    print('    |   |  ')
+    print('  -- --- --')
+    print('    |   |  ')
+    print(f'B {board_strs[1][0]} | {board_strs[1][1]} | {board_strs[1][2]}')
+    print('    |   |  ')
+    print(f'  -- --- --')
+    print('    |   |  ')
+    print(f'C {board_strs[2][0]} | {board_strs[2][1]} | {board_strs[2][2]}')
+
+
+def cli_player(symbol):
+    """The CLI player.
+
+    It takes care of asking the user for moves, responding to invalid
+    moves and printing game result messages.
+
+    This neat solution is implemented using coroutines. To be fair, most
+    students do not know coroutines. Similar functionality can be
+    implemented with OOP or callbacks.
+    """
+    move = None  # For priming.
+    while True:
+        try:
+            board = yield move
+        except IllegalMove as e:
+            print('Illegal move.')
+        except GameFinished as e:
+            if e.winning_symbol == symbol:
+                # We win.
+                print(f'{symbol} wins!')
+            elif e.winning_symbol is None:
+                print('Draw.')
+            else:
+                print(f'{e.winning_symbol} wins this one. Try again!')
+            return
+        else:
+            print(f'You are {symbol}.')
+            print('Current board:')
+            print_board(board)
+        while True:
+            raw_move = input('Choose a move (e.g. B2): ')
+            try:
+                row, col = raw_move
+                row_index = {'A': 0, 'B': 1, 'C': 2}[row]
+                col_index = {'1': 0, '2': 1, '3': 2}[col]
+            except (ValueError, KeyError):
+                print('Invalid format.')
+            else:
+                # `move` gets yielded.
+                move = row_index, col_index
+                break
+
+
+def find_legal_moves(board):
+    for i, row in enumerate(board):
+        for j, cell in enumerate(row):
+            if cell is None:
+                yield i, j
+
+
+def random_max(values, key):
+    if not isinstance(values, tuple):
+        values = tuple(values)
+    max_key = max(map(key, values))  # Might throw ValueError
+    return random.choice(tuple(filter(lambda v: key(v) == max_key, values)))
+
+
+def update(board, move, symbol):
+    succ_board = place_move(board, move, symbol)
+    try:
+        best_Q = -max(Q.get((succ_board, action), 0)
+                      for action in find_legal_moves(succ_board))
+    except ValueError:
+        # No legal moves.
+        best_Q = 0
+    Q[board, move] = best_Q
+
+
+def rl_player(symbol):
+    """The simple player.
+
+    Chooses the first legal move.
+    """
+    move = None  # For priming.
+    last_board = None
+    while True:
+        try:
+            board = yield move
+        except IllegalMove as e:
+            raise RuntimeError('illegal move by AI player') from e
+        except GameFinished as e:
+            assert last_board is not None
+            assert move is not None
+            if e.winning_symbol == symbol:
+                # We win.
+                Q[last_board, move] = 1
+            else:
+                update(last_board, move, symbol)
+            return
+
+        # Update step.
+        if last_board is not None:
+            assert move is not None
+            update(last_board, move, symbol)
+
+        try:
+            move = random_max(find_legal_moves(board),
+                              key=lambda m: Q.get((board, m), 0))
+        except ValueError as e:
+            raise RuntimeError('AI player called with no legal moves') from e
+            
+        last_board = board
+
+def is_legal(move, board):
+    row_i, col_i = move
+    return board[row_i][col_i] is None
+
+
+def find_winner_line(line):
+    """Return the winner of the line if one exists.
+
+    Otherwise return None.
+
+    A line is a row, a column or a diagonal.
+    """
+    if len(line) != 3:
+        raise ValueError('invalid line')
+    symbols = set(line)
+    if len(symbols) == 1:
+        # All equal.
+        return line[0]  # Could be None.
+    return None
+
+
+def find_winner(board):
+    """Return the winner if one exists.
+
+    Otherwise return None.
+    """
+    # We could also use itertools.chain for this.
+    for row in board:
+        winner = find_winner_line(row)
+        if winner is not None:
+            return winner
+    for column in zip(*board):
+        winner = find_winner_line(column)
+        if winner is not None:
+            return winner
+    for diagonal in [(board[0][0], board[1][1], board[2][2]),
+                     (board[0][2], board[1][1], board[2][0])]:
+        winner = find_winner_line(diagonal)
+        if winner is not None:
+            return winner
+    return None
+
+
+def is_finished(board):
+    # Game is finished when the board is full.
+    return all(all(cell is not None for cell in row) for row in board)
+
+
+def inform_game_result(agents, result):
+    for agent in agents:
+        try:
+            agent.throw(GameFinished(result))
+        except StopIteration:
+            pass
+        else:
+            raise RuntimeError('StopIteration expected')
+
+
+def place_move(board, move, symbol):
+    """Place symbol in-place."""
+    x, y = move
+    return tuple(tuple(symbol if j == y else cell
+                       for j, cell in enumerate(row)) if i == x else row
+                 for i, row in enumerate(board))
+
+
+def game(agent1f, agent2f):
+    """Play the game.
+
+    `agent1f` is 'X' and `agent2f` is 'O'. `agent1f` hence begins the
+    game.
+    """
+    board = ((None, None, None),
+             (None, None, None),
+             (None, None, None))
+    
+    agent1 = agent1f('X')
+    next(agent1)  # Prime.
+    agent2 = agent2f('O')
+    next(agent2)  # Prime.
+    
+    turn = False
+    while not is_finished(board):
+        agent_turn = [agent1, agent2][turn]
+        symbol_turn = ['X', 'O'][turn]
+        
+        move = agent_turn.send(board)
+        while not is_legal(move, board):
+            move = agent_turn.throw(IllegalMove)
+
+        board = place_move(board, move, symbol_turn)
+
+        winner = find_winner(board)
+        if winner is not None:
+            inform_game_result([agent1, agent2], winner)
+            return
+
+        turn = not turn
+    else:
+        # No more legal moves but nobody won.
+        inform_game_result([agent1, agent2], None)
+
+
+def main():
+    while True:
+        while True:
+            human_symbol = input('Choose your symbol (X/O): ')
+            if human_symbol in {'X', 'O'}:
+                break
+            else:
+                print('Invalid symbol.')
+        if human_symbol == 'X':
+            agent1f = cli_player
+            agent2f = rl_player
+        else:
+            agent1f = rl_player
+            agent2f = cli_player
+        game(agent1f, agent2f)
+
+
+def pretrain(episodes):
+    for _ in range(episodes):
+        game(rl_player, rl_player)
+
+
+if __name__ == '__main__':
+    pretrain(100)
+    main()
